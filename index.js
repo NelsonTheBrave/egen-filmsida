7c397f0e9ece9a25173dcae5ea74c41e68f3eb70
Nelson <sherman.erik@gmail.com>
2024-01-23 11:17:23 +0100

    started working on egen filmsida project, initial index.js copied from old project

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import fs from 'fs/promises';
+
+const app = express();
+
+// Find index.html
+app.get('/', async (request, response) => {
+  const buf = await fs.readFile('./static/index.html');
+  const html = buf.toString();
+  response.send(html);
+});
+
+app.use('/', express.static('./static'));
+
+app.listen(3080);
