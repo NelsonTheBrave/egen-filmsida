7e94802573000581ebfa747096c74c50aa1846ef
Nelson <sherman.erik@gmail.com>
2024-01-23 12:19:09 +0100

    basic setup for templates using handlebars

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 import express from 'express';
+import expressHandlebars from 'express-handlebars';
 import fs from 'fs/promises';
 
 const app = express();
+app.engine('handlebars', expressHandlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views', './templates');
 
-// Find index.html
+const MENU = [
+  {
+    label: 'Homepage',
+    link: '/',
+  },
+  {
+    label: 'About us',
+    link: '/about',
+  },
+  {
+    label: 'Contact us',
+    link: '/contact',
+  },
+];
+
+async function renderPage(response, page) {
+response.render(page)
+}
+
+// Create routing for index (./) requests
 app.get('/', async (request, response) => {
-  const buf = await fs.readFile('./static/index.html');
-  const html = buf.toString();
-  response.send(html);
+  renderPage(response, 'index');
 });
 
+// Routing for other requests
 app.use('/', express.static('./static'));
 
 app.listen(5080);
