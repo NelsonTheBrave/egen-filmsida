6b826858c5766af981539aa39c47b3742e75e220
Nelson <sherman.erik@gmail.com>
2024-01-25 10:38:56 +0100

    klar sida i stort, återstår integrationstest

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
+// Scope känns som det som fuckar allra mest med mig av allt....
+
 import express from 'express';
 import expressHandlebars from 'express-handlebars';
-import fs from 'fs/promises';
+import { fetchMovie, fetchMovies } from './static/src/filmer.js';
 
 const app = express();
 app.engine('handlebars', expressHandlebars.engine());
 app.set('view engine', 'handlebars');
 app.set('views', './templates');
+
+async function renderPage(response, page, movieList, currentMovie) {
+  let currentPath = (page == 'film') ? 'filmer' : page;
+  currentPath = currentPath == 'index' ? '/' : `/${currentPath}`; // Smidigare sätt att göra detta på för att aktivera menyn för filmer även för enskild film-sida?
+  response.render(page, {
+    menu: MENU.map((item) => {
+      return {
+        label: item.label,
+        link: item.link,
+        status: currentPath == item.link ? 'active' : 'inactive',
+      };
+    }),
+    movies: movieList,
+    movie: currentMovie,
+  });
+}
+
+app.use('/', express.static('./static'));
+app.use('/filmer', express.static('./static')); //Onödigt med två routes?
+
+// MENU STUFF
 
 const MENU = [
   {
@@ -22,29 +45,19 @@ const MENU = [
   },
 ];
 
-async function renderPage(response, page) {
-  response.render(page, { menu: MENU.map(item => {
-    const currentPath = (page == 'index') ? '/' : `/${page}`;
-    return {
-        label: item.label,
-        link: item.link,
-        status: (currentPath == item.link) ? 'active' : 'inactive'
-    }
-  }) });
-}
-
-// Create routing for index (./) requests
-app.get('/', async (request, response) => {
+app.get('/', async (req, response) => {
   renderPage(response, 'index');
 });
-app.get('/filmer', async (request, response) => {
-  renderPage(response, 'filmer');
+app.get('/filmer', async (req, response) => {
+  const movieList = await fetchMovies();
+  renderPage(response, 'filmer', movieList);
 });
-app.get('/evenemang', async (request, response) => {
+app.get('/filmer/:movieId', async (req, response) => {
+  const movie = await fetchMovie(req.params.movieId);
+  renderPage(response, 'film', null, movie);
+});
+app.get('/evenemang', async (req, response) => {
   renderPage(response, 'evenemang');
 });
-
-// Routing for other requests
-app.use('/', express.static('./static'));
 
 app.listen(5080);
---
+++ static/src/filmer.js
@@ -0,0 +1,18 @@
+import fetch from "node-fetch";
+
+export async function fetchMovies() {
+    const res = await fetch(
+      'https://plankton-app-xhkom.ondigitalocean.app/api/movies'
+    );
+    const payload = await res.json();
+    return payload.data;
+  }
+
+export async function fetchMovie(id) {
+    const res = await fetch(
+      `https://plankton-app-xhkom.ondigitalocean.app/api/movies/${id}`
+    );
+    const payload = await res.json();
+    return payload.data;
+  }
+
